9fdb9fc206a96d4ae07490dd07061d6acd75969d
Volodymyr Yepishev <i.m.bwca@gmail.com>
2019-10-20 16:29:00 +0300

    finished

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // require and use "multer"...
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.get('/hello', (req, res) => {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Node.js listening ...');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const responseJSON = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+  res.json(responseJSON);
 });
